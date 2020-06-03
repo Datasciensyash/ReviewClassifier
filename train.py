1644fc9cd27b96ddc7192214d83ed8558b3f9bef
Datasciensyash <60406311+Datasciensyash@users.noreply.github.com>
2020-06-03 20:46:37 +0300

    Update train.py

--- train.py
+++ train.py
@@ -23,6 +23,32 @@ hparams = {
 
 }
 
+def load_pickle(filename:str) -> pd.DataFrame:
+	return pd.read_pickle(filename)
+
+def load_dataframes(filenames_list:list) -> tuple:
+	return [load_pickle(filename) for filename in filenames_list]
+
+def get_sweep_config(init_config:dict) -> dict:
+	wandb.init(project="review_classifier", config=init_config)
+	return wandb.config
+
+def init_vectorizer(min_df:int, max_df:float) -> TfidfVectorizer:
+	return TfidfVectorizer(min_df=min_df, max_df=max_df)
+
+def init_model(solver:str, max_iter:int, random_state:int):
+	return LogisticRegression(solver=solver, max_iter=max_iter, random_state=random_state)
+
+def get_corpus(df_list:list, colname:str='Text') -> np.array:
+	"""
+	Concatenates texts of all dataframes in df_list into corpus.
+	Args:
+		:df_list: list of pd.DataFrame
+		:colname: name of column where text is stored
+	"""
+	return np.concatenate([df['Text'].values for df in df_list])
+
+
 train_file = 'dataset/train.pkl'
 test_file = 'dataset/test.pkl'
 unsup_file = 'dataset/unsup.pkl'
@@ -32,55 +58,51 @@ val_ratio = 0.2
 
 if __name__ == '__main__':
 
-	wandb.init(project="review_classifier", config=hparams)
+	config = get_sweep_config(hparams)
 
-	config = wandb.config #Получаем конфиг sweep'а.
-
-	train_file = 'dataset/train.pkl'
-	unsup_file = 'dataset/unsup.pkl'
-	test_file = 'dataset/test.pkl'
-
-	train_df = pd.read_pickle(train_file)
-	unsup_df = pd.read_pickle(unsup_file)
-	test_df = pd.read_pickle(test_file)
+	train_df, unsup_df, test_df = load_dataframes([train_file, unsup_file, test_file])
 
 	#Make train-val split
 	train_df, val_df = train_test_split(train_df, test_size=val_ratio, random_state=random_state)
 
 	#Vectorizer init
 	if config['vectorizer_fit_unsup']:
-		corpus = np.concatenate([train_df['Text'].values, unsup_df['Text'].values])
+		corpus = get_corpus([train_df, unsup_df], colname='Text')
 	else:
-		corpus = train_df['Text'].values
+		corpus = get_corpus([train_df], colname='Text')
 
-	vectorizer = TfidfVectorizer(min_df=config['min_df'], max_df=config['max_df'])
+	vectorizer = init_vectorizer(min_df=config['min_df'], max_df=config['max_df'])
 
-	#Train features init
+	#Fit vectorizer with train corpus
 	vectorizer = vectorizer.fit(corpus)
 
-	train_features = vectorizer.transform(train_df['Text'])
+	#Get train features from vectorizer
+	train_corpus = get_corpus([train_df], colname='Text')
+	train_features = vectorizer.transform(train_corpus)
 
 	#Model init
-	model = LogisticRegression(solver=config['solver'], max_iter=config['max_iter'], random_state=random_state)
+	model = init_model(solver=config['solver'], max_iter=config['max_iter'], random_state=random_state)
 
 	model = model.fit(train_features, train_df['Target'])
 
 	if config['pseudolabel_unsup']:
-		unsup_features = vectorizer.transform(unsup_df['Text'])
+		unsup_corpus = get_corpus([unsup_df], colname='Text')
+
+		unsup_features = vectorizer.transform(unsup_corpus)
+
 		labels_unsup = model.predict_proba(unsup_features).argmax(1)
 
-		model = LogisticRegression(solver=config['solver'], max_iter=config['max_iter'], random_state=random_state)
+		model = init_model(solver=config['solver'], max_iter=config['max_iter'], random_state=random_state)
 
-		features = vectorizer.transform(np.concatenate([train_df['Text'], unsup_df['Text']]))
+		features = vectorizer.transform(corpus) #This corpus was created earlier.
 
 		model.fit(features, np.concatenate([train_df['Target'], labels_unsup]))
 
 	predictions = model.predict_proba(vectorizer.transform(val_df['Text']))
 
+	#Metrics calculating
 	val_accuracy = accuracy_score(val_df['Target'], predictions.argmax(1))
 	val_logloss = log_loss(val_df['Target'], predictions)
-
-	#Only for information
 	predictions_test = model.predict_proba(vectorizer.transform(test_df['Text']))
 	test_accuracy = accuracy_score(test_df['Target'], predictions_test.argmax(1))
 	test_logloss = log_loss(test_df['Target'], predictions_test)
